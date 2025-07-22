90f1c697eeb0fce8765b44def870698731ec5889
vescafa <vescafa@hotmail.com>
2025-07-22 15:04:11 -0300

    feat: first commit

---
+++ src/main/java/dev/escafa/Events/EventsApiRestApplication.java
@@ -0,0 +1,13 @@
+package dev.escafa.Events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/escafa/Events/EventsApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.escafa.Events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
